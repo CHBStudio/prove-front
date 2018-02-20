33bb3537b51f536c75f347cf209a69e5320d40ff
Антон Чащин <anton.casin@gmail.com>
2018-02-21 01:34:49 +0300

    fix

--- src/components/ReloadButton/index.jsx
+++ src/components/ReloadButton/index.jsx
@@ -1,9 +1,10 @@
 import BaseRoundButton from 'components/BaseRoundButton';
 
-import styles from './styles.scss';
+import styles from './reloadStyles.scss';
 
 
-export default ({ className='', onClick=()=>{} }) => <BaseRoundButton
-  className={cn(styles.root, className)}
-  onClick={onClick}
-/>
+export default ({ className='', onClick=()=>{} }) =>
+  <BaseRoundButton
+    className={cn(styles.root, className)}
+    onClick={onClick}
+  />
