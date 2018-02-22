8c58874d399230b44bda53c073c3e95263f1738e
Антон Чащин <anton.casin@gmail.com>
2018-02-22 03:45:18 +0300

    adaptive

--- src/Root/Landing/Programs/components/DescriptionSide/index.jsx
+++ src/Root/Landing/Programs/components/DescriptionSide/index.jsx
@@ -4,7 +4,9 @@ import styles from './styles.scss';
 export default ({ children, className, isHidden }) => <div
   className={cn(styles.root, isHidden && styles.rootHidden, className)}
 >
-  <div className={styles.wrapper}>
-    { children }
+  <div className={styles.wrapperFlex}>
+    <div className={styles.wrapper}>
+      { children }
+    </div>
   </div>
 </div>
